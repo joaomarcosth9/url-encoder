158f8cfc561ad688caaad4d7a241853799fb7839
joaomarcosth9 <joaomarcosnet10@gmail.com>
2022-01-25 01:54:15 -0300

    fix bugs

--- url_encoder.py
+++ url_encoder.py
@@ -5,8 +5,15 @@ import os
 
 def encode_url(original_url):
     encoded = ''
+    if 'http://' in original_url:
+        original_url = original_url.replace('http://', '')
+    elif 'https://' in original_url:
+        original_url = original_url.replace('https://', '')
     for i in original_url:
-        encoded += hex(ord(i)).replace('0x', '%')
+        if i not in ['/', '+', '?', '&', '#', '=', '.']:
+            encoded += hex(ord(i)).replace('0x', '%')
+        else:
+            encoded += i
     return encoded.upper()
 
 
@@ -14,9 +21,9 @@ try:
     url = input("Insert a URL: ")
     print(encode_url(url))
 
-    command = 'echo {0} | xclip -sel clip'.format(encode_url(url))
+    command = 'echo "{0}" | xclip -sel clip'.format(encode_url(url))
     if os.name in ('nt', 'dos'):
-        command = 'echo {0} | clip.exe'.format(encode_url(url))
+        command = 'echo "{0}" | clip.exe'.format(encode_url(url))
     os.system(command)
 except Exception as error:
     print("Error:", error)
